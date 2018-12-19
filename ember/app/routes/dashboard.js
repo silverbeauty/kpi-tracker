2f3288e131d102093b728a53abc2e57be9eb6c3c
superwebtop <superwebtop@outlook.com>
2018-12-20 00:28:40 +0800

    initial base rails

--- ember/app/routes/dashboard.js
+++ ember/app/routes/dashboard.js
@@ -3,12 +3,12 @@ import AuthenticatedRouteMixin from "ember-simple-auth/mixins/authenticated-rout
 //import {hash} from 'rsvp';
 
 export default Route.extend(AuthenticatedRouteMixin, {
-    // model() {
-    //     return hash(
-    //       {
-    //         categories: this.store.findAll('category'),
-    //         users: this.store.findAll('user'),
-    //         tasks: this.store.findAll('task')
-    //       });
-    //     }
+    model() {
+        return hash(
+          {
+            categories: this.store.findAll('category'),
+            users: this.store.findAll('user'),
+            tasks: this.store.findAll('task')
+          });
+        }
 });
